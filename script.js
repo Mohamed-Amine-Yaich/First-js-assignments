88774a61051f0ab75acdc3725b00ccaf24ba7627
mohamed amine yaich <yaichamine04@gmail.com>
2022-10-12 15:18:58 +0100

    commit

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const computerPlay = () => {
 const playerSelectionFunction = () => {
   let input = "";
   while (true) {
-    /*  console.log(input); */
     input = prompt("Select between Rock Paper and Scissors");
     input = input.toLowerCase().replace(/\b[a-z]/g, function (letter) {
       return letter.toUpperCase();
@@ -35,40 +34,39 @@ const playRound = (playerSelection, computerSelection) => {
     return "You Lose" /*  `You Lose!!${computerSelection} beats ${playerSelection}` */;
   }
 };
-
+/* 
 const playerSelection = playerSelectionFunction();
 const computerSelection = computerPlay();
 console.log("game of one round go..");
-console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection)) */ const game =
+  () => {
+    console.log(
+      "the game is starting you have 5 round to beat the computer go.."
+    );
+    let equality = 0;
+    let roundPlayerWon = 0;
+    let roundComputerWon = 0;
+    for (let i = 0; i < 5; i++) {
+      const playerSelection = playerSelectionFunction();
+      const computerSelection = computerPlay();
+      const roundScore = playRound(playerSelection, computerSelection);
+      console.log(roundScore);
 
-const game = () => {
-  console.log(
-    "the game is starting you have 5 round to beat the computer go.."
-  );
-  let equality = 0;
-  let roundPlayerWon = 0;
-  let roundComputerWon = 0;
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = playerSelectionFunction();
-    const computerSelection = computerPlay();
-    const roundScore = playRound(playerSelection, computerSelection);
-    console.log(roundScore);
-
-    if (roundScore == "You Win") {
-      roundPlayerWon++;
-    } else if (roundScore == "You Lose") {
-      roundComputerWon++;
-    } else if (roundScore == "equality") {
-      equality++;
+      if (roundScore == "You Win") {
+        roundPlayerWon++;
+      } else if (roundScore == "You Lose") {
+        roundComputerWon++;
+      } else if (roundScore == "equality") {
+        equality++;
+      }
     }
-  }
 
-  if (roundPlayerWon > roundComputerWon) {
-    return `You win the game : your win ${roundPlayerWon}and lose${roundComputerWon}and equal round are ${equality} `;
-  } else if (roundPlayerWon == roundComputerWon) {
-    return `equality : your win ${roundPlayerWon} and lose ${roundComputerWon}and equal round are ${equality} `;
-  } else {
-    return `You lose the game : your win ${roundPlayerWon} round and lose ${roundComputerWon} and equal round are ${equality} `;
-  }
-};
-/* console.log(game()); */
+    if (roundPlayerWon > roundComputerWon) {
+      return `You win the game : your win ${roundPlayerWon}and lose${roundComputerWon}and equal round are ${equality} `;
+    } else if (roundPlayerWon == roundComputerWon) {
+      return `equality : your win ${roundPlayerWon} and lose ${roundComputerWon}and equal round are ${equality} `;
+    } else {
+      return `You lose the game : your win ${roundPlayerWon} round and lose ${roundComputerWon} and equal round are ${equality} `;
+    }
+  };
+console.log(game());
